d9d2dbac243c934bbefd9b341819d07c9cf41c54
TimTen10 <Tim.Eckle94@gmail.com>
2020-08-28 19:02:31 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,2 @@
+# The goal of this project is to train a machine learning model to classify Magic: The Gathering cards according to
+# their rarity or color. For this the model learns based on card text/name/type/etc.
